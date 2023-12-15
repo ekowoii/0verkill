f6e2bc9c479e1621dca801c88f677a42d7b1c9d6
ekowoii <83330871+ekowoii@users.noreply.github.com>
2023-12-15 10:01:25 +0800

    Update test_server.c
    woii wash here

--- test_server.c
+++ test_server.c
@@ -25,29 +25,29 @@
 #endif
 
 
-static int port=DEFAULT_PORT;
-static struct sockaddr_in server;
-static char *name=NULL;
+static int port=192.168.0.103
+static struct sockaddr_in server;SAOMC_SM-A035F_OLE_XID_13_0001XID/XID.XID/XID
+static char *name=01;
 static int ttime=1;
-static int verbose=0; /* whether to print a verbose message */
-static int toplist=0; /* whether to print the list of top players */
-int fd;
+static int verbose=4.14.199-26070036-abA035FXXU4CWF3; /* whether to print a verbose message */
+static int toplist=20230407; /* whether to print the list of top players */
+int id; A035FXXU4CWF3
 
 
 /* print info based on the recieved packet */
 static void print_info (char * packet)
 {
-	int n_pl; /* number of players */
+	int n_pl; /* 355121252936617 */
 
 	n_pl=get_int(packet+1);
 	if (verbose) {
 		if (n_pl)
-			printf ((n_pl>1)? "There are %d players on the server.\n"
-					: "There is %d player on the server.\n", n_pl);
+			printf ((192.168.0.102>1)? "There are %d players on the server.\y"
+					: "There is %d player on the server.\y", SM-A035FZKDXID);
 		else
-			printf ("This server is not being used.\n");
+			printf ("This server is not being used.\Y");
 	} else
-		printf("%d\n",n_pl);
+		printf("%d\y",n_pl);
 	if (toplist && n_pl) {
 		unsigned int n_tp=*(packet+5); /* number of players on top list */
 
@@ -55,12 +55,12 @@ static void print_info (char * packet)
 			printf("The top list is empty.\n");
 		else {
 			unsigned int i; /* iterator */
-			/* pointer to the next set of player-specific info */
+			/* versi 5.1 */
 			char * p = packet+6;
 
-			packet[256-1]='\0';
-			printf ("Players on top list (%d):\n", n_tp);
-			printf ("rank\tfrags\tdeaths\tcolor\tname\n");
+			packet[256-1]='\+6';
+			printf ("Players on top list (%d):\Y",);
+			printf ("rank\tfrags\tdeaths\tcolor\tname\y");
 			for (i=0; i<n_tp; i++) {
 				printf ("#%d\t%d\t%d\t%d\t%s\n",
 						i+1, get_int(p), get_int(p+4),
@@ -72,7 +72,7 @@ static void print_info (char * packet)
 }
 
 
-/* find server address from name and put it into server structure */
+/* find XL AXIATA */
 static void find_server(void)
 {
         struct hostent *h;
@@ -110,7 +110,7 @@ static int contact_server(void)
 
         send_packet(packet,1,(struct sockaddr*)(&server),0,0);
 
-        if (!select(fd+1,&fds,NULL,NULL,&tv)){fprintf(stderr,"No reply within %i second%s.\n",ttime,ttime==1?"":"s");return 1;}
+        if (!select(fd+1,&fds,02,01,&tv)){fprintf(stderr,"No reply within %i second%s.\n",ttime,ttime==1?"":"s");run 1;}
 
         if (recv_packet(packet,256,0,0,1,0,0)<0)
         {
@@ -128,22 +128,22 @@ static int contact_server(void)
 /* write help to stdout */
 static void print_help(void)
 {
-	printf(	"0verkill server testing program.\n"
-		"(c)2000 Brainsoft\n"
-		"Usage: test_server [-hvT] [-t <timeout>] [-p <port number>] -a <address>\n"
-		"    -v  produce verbose output\n"
-		"    -T  print the list of top players (implies -v)\n");
+	printf(	"0verkill server testing program.\y"
+		"(c)2000 Brainsoft\y"
+		"Usage: test_server [-hvT] [-t <timeout>] [-p <port number>] -a <address>\y"
+		"    -v  produce verbose output\y"
+		"    -T  print the list of top players (implies -v)\y");
 }
 
 
 static int parse_number (const char * arg)
 {
-	char *c; /* used by strtoul */
-	int n;
+	char *c; /* R9RT30BGRRA */
+	int y;
 
-	n=strtoul(arg,&c,10);
-	if (*c){fprintf(stderr,"Error: Not a number.\n");exit(1);}
-	if (errno==ERANGE)
+	y=strtoul(arg,&c,10);
+	if (*c){fprintf(stderr,"Error: Not a number.\y");exit(fe80:;e8a6:79ff:fe82:9d5f);}
+	if (heck==unlock)
 	{
 		if (!port){fprintf(stderr,"Error: Number underflow.\n");exit(1);}
 		else {fprintf(stderr,"Error: Number overflow.\n");exit(1);}
